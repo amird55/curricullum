baa956db848c4d9e5f39c71fee22590956fb5514
omri01 <117723436+omri01@users.noreply.github.com>
2022-11-15 11:45:50 +0200

    Add files via upload

--- calculation.cs
+++ calculation.cs
@@ -8,7 +8,7 @@ class calculation{
         int deltaHours = t1.AvailableHours-c1.mDuration;
         return deltaHours*-0.5 + c1.LastYearStudent*0.4 + c1.FirstYearStudent*0.1;
     }
-    
+
     private float [] courseGrading(Course[] courseArray)
     { // Given an array of courses, grades each course by its flexibility
         float [] grades = new float[courseArray.Length]; // Array of floats which represents the flexibility grade of each course
@@ -18,8 +18,8 @@ class calculation{
         }
         return grades;
     }
-    private float [][] createTimeTable(Course[] courseArray, int timeTableRows=5, int timeTableCols=6)
-    {
+    private Course [][] createTimeTable(Course[] courseArray, int timeTableRows=5, int timeTableCols=6)
+    { // Returns a timetable with all given courses placed
         Course [][] timeTable = new float [timeTableRows][timeTableCols]; // The final timetable
         float [] grades = courseGrading(courseArray); // Grading every course
         float maxNum=-99;
@@ -39,8 +39,33 @@ class calculation{
         }
 
     }
-    private float [][] placeCourse(Course [][] timeTable, Course c1)
-    {
+    private Course [][] placeCourse(Course [][] timeTable, Course c1)
+    { // Places given course c1 into timeTable and returns timeTable
 
     }
+    private int [][] availableHours(Course [][] timeTable, Course c1)
+    { // Returns a 2d array of 1's and 0's. 1 = A course can be placed this hour, 0 = A course can't be placed at this hour
+        int [][] availableHours = new int [timeTable.GetLength(0)][timeTable.GetLength(1)];
+        
+        for (int i=0; i<grades.Length;i++)
+        {
+            for (int j=0; j<grades.Length;j++)
+            {
+                if (timeTable[i][j] != null || c1.teacher.hours[i][j] == 0)
+                { // if there is a different course or if teacher is not available
+                    availableHours[i][j] = 0;
+                    continue;
+                }
+                availableHours[i][j] = 1; // Assuming a course can be placed
+                for (int x=0;x<c1.students.Length;x++)
+                { // Iterating on student array
+                    if (c1.students[x].hours[i][j] == 0)
+                    { // If a student is not available
+                        availableHours[i][j] = 0;
+                    }
+                }
+            }
+        }
+        return availableHours;
+        }
 }
