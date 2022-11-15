760386d7a4c9b91ef489e5376cf35d7c54fa82a2
Tomer Projects <100287925+tomerProjects@users.noreply.github.com>
2022-11-15 11:56:39 +0200

    Add files via upload

---
+++ Schedule_Students/Schedule_Students/Program.cs
@@ -0,0 +1,69 @@
+﻿using GemBox.Spreadsheet;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // If you are using the Professional version, enter your serial key below.
+        SpreadsheetInfo.SetLicense("FREE-LIMITED-KEY");
+
+        // Definition of JSON file
+        string jsonString = @"{
+                ""0"": {
+                    ""firstName"": ""John"",
+                    ""lastName"": ""Smith"",
+                    ""age"": 27,
+                    ""email"": ""john.smith@gmail.com""
+                },
+                ""1"" : {
+                    ""firstName"": ""Ann"",
+                    ""lastName"": ""Doe"",
+                    ""age"": 25,
+                    ""email"": ""ann.doe@gmail.com""
+                }
+            }";
+
+        // Deserialize JSON string
+        Dictionary<string, User> users = JsonConvert.DeserializeObject<Dictionary<string, User>>(jsonString);
+
+        // Create empty excel file with a sheet
+        ExcelFile workbook = new ExcelFile();
+        ExcelWorksheet worksheet = workbook.Worksheets.Add("Users");
+
+        // Define header values
+        worksheet.Cells[0, 0].Value = "First name";
+        worksheet.Cells[0, 1].Value = "Last name";
+        worksheet.Cells[0, 2].Value = "Age";
+        worksheet.Cells[0, 3].Value = "Email";
+
+        // Write deserialized values to a sheet
+        int row = 0;
+        foreach (User user in users.Values)
+        {
+            worksheet.Cells[++row, 0].Value = user.FirstName;
+            worksheet.Cells[row, 1].Value = user.LastName;
+            worksheet.Cells[row, 2].Value = user.Age;
+            worksheet.Cells[row, 3].Value = user.Email;
+        }
+
+        // Save excel file
+        workbook.Save("JsonToExcel.xlsx");
+    }
+}
+
+class User
+{
+    [JsonProperty("firstName")]
+    public string FirstName { get; set; }
+
+    [JsonProperty("lastName")]
+    public string LastName { get; set; }
+
+    [JsonProperty("age")]
+    public int Age { get; set; }
+
+    [JsonProperty("email")]
+    public string Email { get; set; }
+}
---
+++ Schedule_Students/Schedule_Students/Student.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Schedule_Students
+{
+    public class Student
+    {
+        public int student_id { get; set; }
+        public int gender { get; set; }
+        public string student_fname { get; set; }
+        public string student_lname { get; set; }
+        public string student_mobile { get; set; }
+        public string student_email { get; set; }
+        public string student_college_id { get; set; }
+        public List<int> Course_id { get; set; }
+        public List<int> Course_time { get; set; }
+
+    }
+
+}
