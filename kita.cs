5fc4238739d9416c6df6a1f1012b6a7f3512e2ec
RoyGitPro <117724850+RoyGitPro@users.noreply.github.com>
2022-11-15 11:00:35 +0200

    Add files via upload

---
+++ kita.cs
@@ -0,0 +1,43 @@
+﻿class Kita
+{
+    //pindex powered code
+    public string kita_Name { get; set; }
+    public int kita_Size { get; set; }
+
+    public Kita(string name, int size)
+    {
+        kita_Name = name;
+        kita_Size = size;
+    }
+    public bool DoesItFit(int kita_Size, int studentSize)
+    {
+        if (kita_Size >= studentSize)
+            return true;
+        else
+            return false;
+    }
+    public void Reserve(string kita_name,Courses course_name,DateTime required_course_time)
+    {
+        if (isAvailable(kita_name, required_course_time) == true)
+        {
+            timetable.insert(kita_name, course_name, required_course_time);
+        }
+    }
+    
+    public bool isAvailable(string kita_name,DateTime required_course_time)
+    {
+
+        if (timetable.required_course_time== null)
+        {
+            return true;
+        }
+        return false;
+    }
+
+
+    //לדאוג שהחדר מתאים למספר התלמידים הרצוי באותו הרגע
+    // פונקציה האם החדר פנוי
+    // פונקציה אשר שומרת את החדר לקורס מסויים
+    //לשייך כיתה וגודלה
+    //האם החדר פנוי לזמן מסויים
+};
